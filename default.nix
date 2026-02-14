5391741ccbf790c5556648171d0b7c399f6f6bd9
Sam <30577766+Samasaur1@users.noreply.github.com>
2026-02-13 21:59:45 -0700

    nix: update packaging

--- default.nix
+++ default.nix
@@ -1,25 +1,19 @@
-{ lib, rustPlatform, stdenv, darwin }:
+{ lib, rustPlatform, ... }:
 
 let cargoToml = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package;
 
-in rustPlatform.buildRustPackage rec {
-  pname = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.name;
-  version =
-    (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+in rustPlatform.buildRustPackage {
+  pname = cargoToml.name;
+  version = cargoToml.version;
 
   src = ./.;
 
-  useFetchCargoVendor = true;
   cargoHash = "sha256-FDVvZYJmYtSU8UAhGm02QgpVBDkKplA70TohoWXCtZ0=";
-
-  buildInputs = [ ] ++ lib.optionals stdenv.isDarwin [ ];
 
   env = { VERGEN_IDEMPOTENT = true; };
 
-  meta = with lib; {
+  meta = {
     description = "Generate new Factor! puzzles";
     homepage = "https://factor.samasaur.com";
-    license = with licenses; [ ];
-    maintainers = with maintainers; [ ];
   };
 }
